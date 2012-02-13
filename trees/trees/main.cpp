b35b6be15a042cc2b53d62ebdca024eece855a7d
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2012-02-13 23:12:42 +0300

    Base TREE, default test 	new file:   trees/trees/main.cpp 	new file:   trees/trees/test.in 	new file:   trees/trees/trees.pro 	new file:   trees/trees/trees.pro.user

---
+++ trees/trees/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const char  fInName [] = "test.in";
+const char  fOutName [] = "test.out";
+
+struct Node
+{
+    int key;
+    Node* lChild;
+    Node* rChild;
+};
+
+Node* find (int x, Node* node)
+{
+    if (node == NULL) return NULL;
+    else
+        if (x<node->key) find (x,node->lChild);
+        else if (x>node->key) find (x, node->rChild);
+            else return node;
+}
+
+void add (int x, Node* node)
+{
+    if (node==NULL)
+    {
+        node = new Node;
+        node->lChild=NULL;
+        node->rChild=NULL;
+        node->key=x;
+    }
+    else
+    {
+        if (x<node->key)
+        {
+            add(x,node->lChild);
+        }
+        else
+            if (x>node->key)
+            {
+                add(x,node->rChild);
+            }
+    }
+}
+
+Node* findMinimum (Node* node)
+{
+    if (node == NULL) return NULL;
+    else
+        if (node->lChild==NULL)
+        {
+            return node;
+        }
+        else
+        {
+            return findMinimum(node->lChild);
+        }
+}
+
+Node* findMaximum (Node* node)
+{
+    if (node == NULL) return NULL;
+    else
+        if (node->rChild==NULL)
+        {
+            return node;
+        }
+        else
+        {
+            return findMinimum(node->rChild);
+        }
+}
+
+bool remove (int x, Node* node)
+{
+    if (node==NULL)
+    {
+        return false;
+    }
+    else
+    {
+        if (x<node->key)
+        {
+           return  remove(x,node->lChild);
+        }
+        else if (x>node->key)
+        {
+            return remove(x,node->rChild);
+        }
+            else
+            {
+                if(node->lChild==NULL)
+                {
+                    Node* tmp = node;
+                    node= node->rChild;
+                    delete tmp;
+                }
+                else
+                    if (node->rChild==NULL)
+                    {
+                        Node* tmp = node;
+                        node = node->lChild;
+                        delete tmp;
+                    }
+                    else
+                    {
+                        Node* tmp =findMinimum(node->rChild);
+                        node->key=tmp->key;
+                        remove(node->key,node->rChild);
+                    }
+            }
+    }
+}
+
+int main ()
+{
+    return 0;
+}
---
+++ trees/trees/trees.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-02-13T22:21:22
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = trees
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+OTHER_FILES += \
+    test.in
