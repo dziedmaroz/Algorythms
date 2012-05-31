63d0455c77e192c84185425d7f57b561dc0ef3c9
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2012-05-31 18:32:39 +0300

    fast dynamics solution

--- dynamics/dynamics/main.cpp
+++ dynamics/dynamics/main.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
-#include <string>
-#include <map>
-#include <set>
 #include <stack>
 using namespace std;
 //                     a    b    c    d    e    f    g    h    i    j    k    l    m    n    o    p    q    r    s    t    u    v    w    x    y    z
@@ -14,69 +10,68 @@ const char file_out[] = "output.txt";
 
 
 struct Node
-{   
-    //char chNum;
-    map<char, Node*> children;
+{
+    Node* children [10];
     bool wrdBegin;
+    int id;
 };
 
-string wordToNumber (string word)
+char* wordToNumber (char* word)
 {
-    string num = word;
-    for (int i=0;i<word.length();i++)
+    char* num = new char [strlen(word)+1];
+    for (int i=0;i<strlen(word);i++)
     {
         if (word[i]>='A' && word[i]<='Z')
         {
             num[i] = table[word[i]-'A'];
         }
+        else
+        {
+            num[i]=word[i];
+        }
     }
+    num[strlen(word)]='\0';
     return num;
 }
 
 
-void addWord (string word, Node* &node, int pos)
+void addWord (char* word, Node* &node, int pos, int id)
 {
     pos--;
-    if (node->children[word[pos]]==NULL)
+    if (node->children[word[pos]-'0']==NULL)
     {
-        node->children[word[pos]]=new Node;
-        node->children[word[pos]]->wrdBegin = false;       
+        node->children[word[pos]-'0']=new Node;
+        for (int i=0;i<10;i++) node->children[word[pos]-'0']->children[i]=NULL;
+        node->children[word[pos]-'0']->wrdBegin = false;
     }
     if (pos==0)
     {
-        node->children[word[pos]]->wrdBegin = true;
+        node->children[word[pos]-'0']->wrdBegin = true;
+        node->children[word[pos]-'0']->id = id;
     }
     else
     {
-        addWord (word,node->children[word[pos]],pos);
+        addWord (word,node->children[word[pos]-'0'],pos, id);
     }
 }
 
-bool contains (string word, Node* &node, int pos)
-{
-    if (pos==0)
-    {
-        return node->wrdBegin;
-    }
-    pos--;
-    if (node->children[word[pos]]!=NULL) return contains(word,node->children[word[pos]],pos);
-    return false;
-}
 
 void clear (Node* &node)
 {
-    map<char, Node*>::iterator iter;
-    for (iter = node->children.begin(); iter!=node->children.end();iter++)
+    for (int i=0;i<10;i++)
     {
-        if (iter->second!=NULL) clear(iter->second);
+        if (node->children[i]!=NULL) clear (node->children[i]);
+        delete node->children[i];
+
     }
-    delete  node;
 }
 int main ()
 {
     Node* root = new Node;
+    for (int i=0;i<10;i++) root->children[i]=NULL;
     FILE* fin = fopen (file_in,"r");
-    map<int, map<string , string> > words;  
+   // map<int, map<int , string> > words;
+    char** wordArr = new char*[50010];
     char number [10010];
     int resTable [10010][2];
     fscanf(fin,"%s",&number);
@@ -87,38 +82,38 @@ int main ()
     }
     int wordCount = 0 ;
     fscanf (fin,"%d",&wordCount);
-    string word;
-    string num;
+    char* num=NULL;
     for (int i=0;i<wordCount;i++)
     {
-        char tmpStr [110];
-        fscanf (fin,"%s",&tmpStr);
-        word = tmpStr;
-        num = wordToNumber(word);
-        addWord(num,root,num.length());
-        words[num.length()][num]=word;
+        wordArr[i] = new char[110];
+        fscanf (fin,"%s",wordArr[i]);
+        wordArr[i];
+        num = wordToNumber(wordArr[i]);
+        addWord(num,root,strlen(num), i);
+        delete [] num;
     }
+
     fclose (fin);
     for (int i=0;i<strlen(number);i++)
     {
         int k = i;
         Node* node = root;
-        while (k>=0 && k<=i &&  node->children[number[k]]!=NULL)
+        while (k>=0 && k<=i &&  node->children[number[k]-'0']!=NULL)
         {
-            node = node->children[number[k]];
+            node = node->children[number[k]-'0'];
             if (node->wrdBegin)
             {
                 if (k==0)
                 {
                     resTable[i][0]=1;
-                    resTable[i][1]=0;
+                    resTable[i][1]=node->id;
                 }
                 else
                 {
                     if (resTable[k-1][0]!=0 && (resTable[i][0] > resTable[k-1][0]+1 || resTable[i][0]==0))
                     {
                         resTable[i][0] = resTable[k-1][0]+1;
-                        resTable[i][1] = k ;
+                        resTable[i][1] = node->id;
                     }
                 }
             }
@@ -130,18 +125,16 @@ int main ()
     if (resTable[strlen(number)-1][0]!=0)
     {
         fprintf (fout, "%d\n",resTable[strlen(number)-1][0]);
-
-        stack<string> wrds;
-        string tmpStr = string(number);
+        stack<int> wrds;
         int k=strlen(number)-1;
         while (k>=0)
         {
-            wrds.push(words[k-resTable[k][1]+1][tmpStr.substr(resTable[k][1],k-resTable[k][1]+1)]);
-            k = resTable[k][1]-1;
+            wrds.push(resTable[k][1]);
+            k =k - strlen(wordArr[resTable[k][1]]);
         }
         while (!wrds.empty())
         {
-            fprintf(fout,"%s",wrds.top().c_str());
+            fprintf(fout,"%s",wordArr[wrds.top()]);
             wrds.pop();
             if (!wrds.empty())
             {
@@ -154,6 +147,7 @@ int main ()
         fprintf (fout,"No solution\n");
     }
     fclose (fout);
+    delete [] wordArr;
     clear (root);
     return 0;
 }
